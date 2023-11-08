334690709540d65fc92a5ce5dc53612c53510729
Jeffrey Ng <jeffreyjng@hotmail.com>
2023-11-08 11:58:09 -0500

    added more survey questions unfinished

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ responses=[]
 def start_survey():
     return render_template('home.html')
 
-@app.route('/questions/<int:num>')
+@app.route('/satisfaction-questions/<int:num>')
 def display(num):
     q= surveys['satisfaction'].questions[num]
     ans=Question(q, choices=None)
@@ -25,24 +25,49 @@ def display(num):
     return render_template('/questions.html', survey_q=q.question, answer0=ans.choices[0], 
             answer1=ans.choices[1])
 
-@app.route('/questions/<int:num>', methods=['POST'])
+@app.route('/satisfaction-questions/<int:num>', methods=['POST'])
 def answer(num):
     session['response']=request.form['tally']
     responses.append(session['response'])
     print(responses)
-    return redirect('/questions/{}'.format(num+1))
+    return redirect('/satisfaction-questions/{}'.format(num+1))
 
-@app.route('/questions/2')
+@app.route('/satisfaction-questions/2')
 def show():
     stuff=Question(question="On average, how much do you spend a month on frisbees?", choices=[['Less than $10,000'], ['$10,000 or more']])
     return render_template('oddQ.html', odd_q= stuff.question, answer2=stuff.choices[0], answer3=stuff.choices[1] )
 
-@app.route('/questions/4')
+@app.route('/satisfaction-questions/4')
 def end():
     return render_template('thank_you.html')
     
 
 
+##### HERE IS THE CODE FOR PERSONALITY SURVEY #####
 
+@app.route('/personality-questions/<int:num>')
+def display1(num):
+    q= surveys['personality'].questions[num]
+    ans=Question(q, choices=None)
+    return render_template('/questions.html', survey_q=q.question, answer0=ans.choices[0], 
+            answer1=ans.choices[1])
 
+@app.route('/personality-questions/<int:num>', methods=['POST'])
+def answer1(num):
+    session['response']=request.form['tally']
+    responses.append(session['response'])
+    print(responses)
+    return redirect('/personality-questions/{}'.format(num+1))
+@app.route('/personality-questions/2', methods=['POST'])
+def answer2(num):
+    q=surveys['personality'].questions[num]
+    ans=Question(q[0])
+    ans2=Question(q[1])
+    return redirect('/personality-questions/{}'.format(num+1), ans)
+@app.route('/personality-questions/3', methods=['POST'])
+def answer3(num):
     
+    session['response']=request.form['tally']
+    responses.append(session['response'])
+    print(responses)
+    return redirect('/personality-questions/{}'.format(num))
